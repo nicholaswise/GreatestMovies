682228075d227c5dd0b9abd7dc109a11fe331bdb
Nick Wise <nickcwise@gmail.com>
2017-07-26 15:17:46 -0400

    stuff

---
+++ GreatestMovies/Models/Genre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GreatestMovies.Models
+{
+    public class Genre
+    {
+        [Key]
+        public int GenreID { get; set; }
+        public string GenreName { get; set; }
+
+        public virtual ICollection<Movie> Movies { get; set; }
+
+    }
+}
